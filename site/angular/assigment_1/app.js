3ca36fc762fe4a1af8f2384a7f4a26f3a7d5f169
mykolapasko <nikpasko@gmail.com>
2016-09-28 00:59:25 +0300

    bonus added

--- site/angular/assigment_1/app.js
+++ site/angular/assigment_1/app.js
@@ -2,32 +2,40 @@
 'use strict';
 
 angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController); 
+.controller('LunchCheckController', LunchCheckController);
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.message = "";
   $scope.input_string = "";
+  $scope.messageClass = "";
+  $scope.borderClass = "";
 
   $scope.displayMessage = function () {
   	var counter = getNumber($scope.input_string);
   	if (counter === 0) {
-	$scope.message = "Please enter data first";
-	} else if (0 < counter && counter < 4) {
-	$scope.message = "Enjoy!";
-	} else {
-	$scope.message = "Too much!";
-	};
+      $scope.message = "Please enter data first";
+      $scope.messageClass = "text-danger";
+      $scope.borderClass = "has-error";
+    	} else if (0 < counter && counter < 4) {
+    	$scope.message = "Enjoy!";
+      $scope.messageClass = "text-success";
+      $scope.borderClass = "has-success";
+    	} else {
+    	$scope.message = "Too much!";
+      $scope.messageClass = "text-success";
+      $scope.borderClass = "has-success";
+    };
 
 	function getNumber(str) {
 		var normalized = str.split(",").filter(function (a) {
 		return (a !== " ") && (a !== "")
 		});
-	return (normalized[0] === str) ? 0 : normalized.length
-	};	
+    console.log(normalized.length);
+	return normalized.length
+	};
+  console.log($scope.messageStyle);
+  console.log(getNumber($scope.input_string));
 };
 };
-
 })();
-
-
